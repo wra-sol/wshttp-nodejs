4980239df57cc7a3772636e3cefe4632fb930b18
wra-sol <88047072+wra-sol@users.noreply.github.com>
2024-04-25 12:28:00 -0400

    initial commit

--- server.mjs
+++ server.mjs
@@ -1,6 +1,23 @@
-import { createServer } from 'http';
+import https from 'https';
+import { WebSocketServer } from 'ws';
+import express from 'express'
 
-createServer((req, res) => {
-  res.write('Hello World!');
-  res.end();
-}).listen(process.env.PORT);
+const app = express();
+
+const server = https.createServer(app);
+
+const wss = new WebSocketServer({ server });
+wss.on('connection', function connection(ws) {
+  console.log('Secure client connected');
+  ws.on('message', function incoming(message) {
+    console.log('received: %d bytes', message.length);
+  });
+
+  ws.on('close', () => console.log('Client disconnected'));
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+})
+
+server.listen(process.env.PORT);
